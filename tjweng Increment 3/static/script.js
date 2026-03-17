edf7aed4e50f3888b1eaf1a5a03d3c71f3ddbd34
Tommy Weng <thomasjweng2019@gmail.com>
2026-03-16 20:44:16 -0400

    Add files via upload

---
+++ tjweng Increment 3/static/script.js
@@ -0,0 +1,16 @@
+var x = 7;
+var y = 5;
+var z = x + y;
+// console.log(z);
+
+// var A = "hello ";
+// var B = "world";
+// var C = A + B;
+// console.log(C);
+
+function myFirstFunction(x1, x2){
+    var x3 = x1 + x2;
+    console.log(x3);
+}
+
+myFirstFunction(x, y);
